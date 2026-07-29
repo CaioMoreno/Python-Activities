67ba74fc049ac7bd4610014692d4d077e81a1661
Caio Moreno <caiomoreno.caio@gmail.com>
2026-07-29 03:34:56 -0300

    Delete vscode/mooc-programming-25 directory

--- vscode/mooc-programming-25/part07-04_lottery_numbers/src/lottery_numbers.py
+++
@@ -1,9 +0,0 @@
-# Write your solution here
-from random import *
-
-def lottery_numbers(amount: int, lower: int, upper: int):
-    number_pool = list(range(lower, upper+1))
-    weekly_draw = sample(number_pool, amount)
-    order=sorted(weekly_draw)
-    return order
-   
